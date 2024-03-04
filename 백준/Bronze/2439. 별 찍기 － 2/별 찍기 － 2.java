452c528711e4008fc8ed0576d714b8a0f255d3ed
dragoncastle0428 <143994426+dragoncastle0428@users.noreply.github.com>
2024-03-05 00:09:00 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 376 ms, Memory: 18612 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for (int i = 0; i < num; i++){
+            for (int j = 0; j < num - i -1; j++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
