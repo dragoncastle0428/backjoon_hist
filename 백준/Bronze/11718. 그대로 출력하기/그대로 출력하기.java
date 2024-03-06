b926a479d88da7d0bc34e52e6e86b41284b8bd1d
dragoncastle0428 <143994426+dragoncastle0428@users.noreply.github.com>
2024-03-07 00:29:52 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        while(s.hasNext()){
+            String input = s.nextLine();
+
+            System.out.println(input);
+        }
+    }
+}
