7e94e012be27830a3cfd5c5bfa09836e09d3d7e4
dragoncastle0428 <143994426+dragoncastle0428@users.noreply.github.com>
2024-03-06 23:32:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        char input = s.nextLine().charAt(0);
+
+        System.out.println((int)input);
+    }
+}
