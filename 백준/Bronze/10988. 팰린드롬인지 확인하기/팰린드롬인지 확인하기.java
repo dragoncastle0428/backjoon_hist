7de8979f95df61c8ebb8272da8c6de6a9a4c0825
dragoncastle0428 <143994426+dragoncastle0428@users.noreply.github.com>
2024-03-07 21:45:36 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,38 +1,18 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        String input = s.nextLine();
-        String comp = input;
+        String str = s.nextLine();
 
-        for (int i = 0; i < input.length()/2; i++){
-            if (i > 0){
-                String temp1 = comp.substring(0, i);
-                String temp2 = comp.substring(i, i+1);
-                String temp3 = comp.substring(input.length()-i);
-                String temp4 = comp.substring(input.length()-i-1, input.length()-i);
-                String temp5 = comp.substring(i+1, input.length()-i-1);
-
-                comp = (((temp1.concat(temp4)).concat(temp5)).concat(temp2)).concat(temp3);
-
-            }
-            else{
-                String temp1 = comp.substring(0, 1);
-                String temp2 = comp.substring(input.length()-1);
-                String temp3 = comp.substring(1, input.length()-1);
-
-                comp = (temp2.concat(temp3)).concat(temp1);
-            }
-        }
-
-        if (input.equals(comp)){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(0);
+        StringBuffer sb = new StringBuffer(str);
+        if(str.equals(sb.reverse().toString())) {
+            System.out.println("1");
+        }else {
+            System.out.println("0");
         }
     }
+
 }
