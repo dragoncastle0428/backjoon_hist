c62b3f401376d86a58f54c23d478daadcfb2cbca
dragoncastle0428 <143994426+dragoncastle0428@users.noreply.github.com>
2024-03-14 22:55:03 +0900

    Update 줄 세우기.py

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -4,10 +4,10 @@ from collections import deque
 #int 자료형 2개 입력
 n, m = map(int, sys.stdin.readline().split())
 
-graph = [[] for _ in range(n+1)]
-degree = [0 for _ in range(n+1)]
-list = []
-anw = []
+graph = [[] for _ in range(n+1)] #자신보다 큰 숫자와 연결 (한방향)
+degree = [0 for _ in range(n+1)] #자신보다 작은 숫자의 갯수
+list = [] #자신보다 작은 수가 없는 경우 (degree = 0)
+anw = [] #최종 출력하는 리스트
 
 for i in range(m):
     num1, num2 = map(int, sys.stdin.readline().split())
