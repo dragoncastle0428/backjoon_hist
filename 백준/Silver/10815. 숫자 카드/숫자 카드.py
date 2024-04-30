12ae0a68251851759d97e03e6744be30a53c85f7
dragoncastle0428 <143994426+dragoncastle0428@users.noreply.github.com>
2024-04-30 11:32:55 +0900

    [Silver V] Title: 숫자 카드, Time: 752 ms, Memory: 115372 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,25 +1,16 @@
 import sys
 
 n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
-a.sort()
+n = list(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
-b = list(map(int, sys.stdin.readline().split()))
-result = []
+m = list(map(int, sys.stdin.readline().split()))
+sets = {}
 
-for i in b:
-    start = 0
-    end = n-1
-    check = 0
-    while (start <= end):
-        mid = int((start+end)/2)
-        if a[mid] == i:
-            check = 1
-            break
-        elif a[mid] > i:
-            end = mid-1
-        else:
-            start = mid+1
-    result.append(check)
+for i in n:
+    sets[i] = 1
 
-print(*result)
+for i in m:
+    if i in sets:
+        print(1, end= ' ')
+    else:
+        print(0, end= ' ')
