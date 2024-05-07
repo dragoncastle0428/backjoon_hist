7e17c27e3e4e3fec1005e7915b510ad0d775a82f
이용성 <143994426+dragoncastle0428@users.noreply.github.com>
2024-05-07 23:47:51 +0900

    [Silver II] Title: 베르트랑 공준, Time: 264 ms, Memory: 35248 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,35 +1,27 @@
-from math import *
-import sys
+import math
 
-def prime(num):
-    if num <= 1:
-        return 0
-    if num % 2 == 0:
-        return 0
-    for i in range(3, int(sqrt(num)) + 1, 2):
-        if num % i == 0:
-            return 0
-    return 1
+def sieve_of_eratosthenes(max_limit):
+    prime = [True for _ in range(max_limit+1)]
+    prime[0] = prime[1] = False
+    for i in range(2, int(math.sqrt(max_limit)) + 1):
+        if prime[i]:
+            for j in range(i*i, max_limit+1, i):
+                prime[j] = False
+    return prime
 
+def count_primes_in_range(n, prime):
+    count = 0
+    for i in range(n+1, 2*n+1):
+        if prime[i]:
+            count += 1
+    return count
+
+# 범위 내의 최대 값으로 에라토스테네스의 체를 한 번만 생성
+max_limit = 123456 * 2
+prime = sieve_of_eratosthenes(max_limit)
 
 while True:
-    n = int(sys.stdin.readline())
-
+    n = int(input())
     if n == 0:
         break
-
-    m = n + 1
-    result = 0
-
-    if n < 2:
-        result = 1
-
-    if m % 2 == 0:
-        m += 1
-
-    n = 2 * n + 1
-
-    for i in range(m, n, 2):
-        result += prime(i)
-
-    print(result)
+    print(count_primes_in_range(n, prime))
