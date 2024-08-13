04b772895a34620fc637f12966d884bee28330e0
이용성 <143994426+dragoncastle0428@users.noreply.github.com>
2024-08-13 21:17:38 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 36580 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+import sys
+
+result = []
+
+def function(array, num):
+    if num == 0:
+        result.append(array)
+    else:
+        for i in range(1, n+1):
+            if i not in array:
+                array2 = array + [i]
+                function(array2, num - 1)
+
+n, m = map(int, sys.stdin.readline().split())
+function([], m)
+
+for i in result:
+    print(*i)
